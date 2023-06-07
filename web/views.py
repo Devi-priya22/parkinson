1a416a59dbc9c5142b54f2916a002e0af5908751
akkupy <akkupy@proton.me>
2023-06-07 14:24:12 +0530

    Renamed the code

--- web/views.py
+++ web/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from ..ml.park import mach
 # Create your views here.
 
 
 def input(request):
+    if request.method == 'POST':
+
+
+
+
+
+
+
+        
+        pass
     return render(request,'input.html')
